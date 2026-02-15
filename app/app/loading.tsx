4b46e2576e5c81bc20d09cc2d525741ebcd9322e
theimpactlab <ryan@theimpactlab.co.uk>
2026-02-15 20:08:27 +0000

    Add loading spinner for tab transitions

---
+++ app/app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[60vh]">
+      <div className="relative w-12 h-12">
+        <div
+          className="absolute inset-0 rounded-full border-2 border-white/10"
+        />
+        <div
+          className="absolute inset-0 rounded-full border-2 border-transparent border-t-white/70 animate-spin"
+        />
+      </div>
+      <p className="mt-4 text-white/30 text-xs uppercase tracking-widest">Loading</p>
+    </div>
+  );
+}
